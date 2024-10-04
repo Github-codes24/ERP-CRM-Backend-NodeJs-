0934cefda3cc65db20c2ba11e3dc83515494d26f
anujtembhurne-ai <anuj.tembhurne@technokrate.com>
2024-10-04 10:26:19 +0530

    finance to post

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/finance", postRouter);
+app.use("/api/post", postRouter);
 
 app.use("/api/customer", customerRouter);
 app.use("/api/product", productRouter);
