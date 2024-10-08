9144b46e90394a5a8138a4ba11a8afbccfb42063
anujtembhurne-ai <anuj.tembhurne@technokrate.com>
2024-10-08 23:49:22 +0530

    cors code added

--- index.js
+++ index.js
@@ -15,14 +15,7 @@ const postRouter = require("./routes/posts.js");
 const app = express();
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:3000', 'https://yourdomain.com'];
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: ['http://localhost:3000', 'http://localhost:5173'],
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
