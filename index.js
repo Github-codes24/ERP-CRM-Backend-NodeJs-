6525d7374fe9fe151260ef5191a048f7fa6a9bb8
anujtembhurne-ai <anuj.tembhurne@technokrate.com>
2024-10-09 15:37:12 +0530

    localhost 5174 added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const postRouter = require("./routes/posts.js");
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174'],
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
